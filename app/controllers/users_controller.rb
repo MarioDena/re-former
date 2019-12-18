80d92840b82331384ea3008845a7714311d66ef3
mike <michgoldennathan17@gmail.com>
2019-12-18 02:19:32 +0100

    [Feature] Add create acton in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,13 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to new-user_path
+    else
+      render :new
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validate :username, presence: true, uniqueness: true, length {minimum: 6}
-    validate :password, presence: true, length {minimum: 6}
-    validate :email, presence: true, uniqueness: true length {maximum: 255}
+    validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
 end
