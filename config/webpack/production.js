199f533b5d90e4f0fb4d2a90b6a0f6df37742fc4
MarioDena <enrique.dn@outlook.com>
2019-12-18 08:48:46 -0600

    [Update] updated yarn-webpacker

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-
->>>>>>> ccc135bc0726667c6274a423b668a18fba58d71f
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
