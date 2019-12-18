468d35dee533d3b255c5810bf3637a3f226fb22f
mike <michgoldennathan17@gmail.com>
2019-12-18 21:15:27 +0100

    [Fix]Correct rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@
 
 class UsersController < ApplicationController
   def new
-  @user = User.new
+    @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
@@ -28,12 +28,10 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-   
-  
+
   private
+
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:username, :email, :password)
   end
-
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
-    validates :password, presence: true, length: { minimum: 6 }
-    validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
 end
